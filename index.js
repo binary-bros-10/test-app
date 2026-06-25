a9503ee9c406d244ec5b29642f3b80891ccbe9a6
Karan Harsh Singh <ask.geminiservices@gmail.com>
2026-06-25 22:32:40 +0530

    updated

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
 app.get("/", (req, res) => {
   res.json({
+    message: "Hello from DeployIt!",
     DATABASE_URL: process.env.DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET,
+    API_KEY: process.env.API_KEY,
   });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
